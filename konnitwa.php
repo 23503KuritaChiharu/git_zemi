b37c712e53a6d47ecdf4bf2c8c694c8967a377ad
chr411 <18412@toba.kosen-ac.jp>
2021-06-04 14:05:12 +0900

    konnitiwa.phpを追加

---
+++ konnitwa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+  <meta charset="utf-8">
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>2021teichiami-display</title>
+ </head>
+ 
+ <body>
+	 <div>
+		<h2>こんにちは</h2>
+	 </div>
+ </body>
